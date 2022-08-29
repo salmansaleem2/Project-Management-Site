9cff1702e244efef9d5c920c7605d6a2e5855402
salmansaleem2 <salmansaleem8523@gmail.com>
2022-08-29 18:59:17 +0500

    added Handling File Inputs and firebase storage setup

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -8,9 +8,39 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (!selected) {
+      setThumbnailError("Please select a file");
+      return;
+    }
+    if (!selected.type.includes("image")) {
+      setThumbnailError("Selected file must be an image");
+      return;
+    }
+    if (!selected.size > 100000) {
+      setThumbnailError("Image file size must be less than 100kb");
+      return;
+    }
+    setThumbnailError(null);
+    setThumbnail(selected);
+
+    console.log("thumbnail updated ");
+  };
 
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={handleSubmit}>
       <h2>Sign up</h2>
       <label>
         <span>email:</span>
@@ -38,14 +68,11 @@ const Signup = () => {
           onChange={(e) => setDisplayName(e.target.value)}
           value={displayName}
         />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
       <label>
         <span>Profile thumbnail:</span>
-        <input
-          required
-          type="file"
-          onChange={(e) => setThumbnail(e.target.value)}
-        />
+        <input required type="file" onChange={handleFileChange} />
       </label>
       <button className="btn">Sign up</button>
     </form>
