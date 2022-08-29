ef041081235fc985dbb9f291c530dbe1ed69c415
salmansaleem2 <salmansaleem8523@gmail.com>
2022-08-29 18:09:46 +0500

    added signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,8 +1,55 @@
+import { useState } from "react";
+
 // styles
 import "./Signup.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>display Name:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Profile thumbnail:</span>
+        <input
+          required
+          type="file"
+          onChange={(e) => setThumbnail(e.target.value)}
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 };
 
 export default Signup;
