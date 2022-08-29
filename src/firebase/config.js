c544ea6c8d7e2b5f92a470b4a845af27a76d00f8
salmansaleem2 <salmansaleem8523@gmail.com>
2022-08-29 15:49:06 +0500

    added firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjp9FTW3EBXrNk7Y4Y7-B_M-wt5heJXAc",
+  authDomain: "thedojosite-cfd82.firebaseapp.com",
+  projectId: "thedojosite-cfd82",
+  storageBucket: "thedojosite-cfd82.appspot.com",
+  messagingSenderId: "341863479380",
+  appId: "1:341863479380:web:a10a0d1663d64b602795c1",
+};
+
+//  init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
